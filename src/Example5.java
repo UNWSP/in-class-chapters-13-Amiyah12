94bf75e03fc3895cee5b79f46674baadf975db5b
Amiyah12 <37642477+Amiyah12@users.noreply.github.com>
2022-11-10 20:41:19 -0600

    Example5

---
+++ src/Example5.java
@@ -0,0 +1,27 @@
+import java.nio.file.*;
+import java.util.Scanner;
+import java.io.*;
+public class Example5 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		Path file = 
+				Paths.get("C:\\Users\\drsmi\\IdeaProjects\\Java Course\\Chapter13\\src\\CustomerList.txt);"
+			Scanner kb = new Scanner(System.in);
+			String[] array = new String[3];
+			String s = "";
+			String delimiter = ",";
+			String id;
+			
+			String firstName;
+			String lastName;
+			
+			double balance;
+			
+			final String QUIT = "ZZZ";
+
+	}
+
+}
